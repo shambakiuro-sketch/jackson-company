0d0a112b05aa8b37bec17286e7db2bc2107e1050
shambakiuro-sketch <shambakiu.r.o@gmail.com>
2026-08-08 00:54:31 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
-import Portfolio from './components/Portfolio';
+import Process from './components/Process';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -16,7 +16,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
-      <Portfolio />
+      <Process />
       <Contact />
       <Footer />
     </>
